8414215c06f4e077d6bbff6f9e942cf2c324085e
sontva-2630 <tran.van.anh.son@sun-asterisk.com>
2022-06-26 22:37:45 +0700

    init ui

---
+++ src/components/header/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.scss'
+
+export default function Header() {
+  return (
+    <div className='header header-container'>
+      <button className='btn btn--header'>Create New Task</button>
+      <div style={{
+        display: 'flex',
+        gap: '20px'
+      }}>
+        <input className='input input--header' type={'text'} placeholder={'Type some thing to search...'} />
+        <button className='btn btn--header'>Search</button>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/sidebar/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.scss'
+
+export default function Sidebar() {
+  return (
+    <div className='sidebar sidebar-container'>
+      <ul>
+        <li><a href='/'>All Task</a></li>
+        <li><a href='/'>All Task</a></li>
+        <li><a href='/'>All Task</a></li>
+        <li><a href='/'>All Task</a></li>
+      </ul>
+    </div>
+  )
+}
