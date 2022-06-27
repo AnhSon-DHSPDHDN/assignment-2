a391848d08cac029607609a29ec899bd2552ce99
sontva-2630 <tran.van.anh.son@sun-asterisk.com>
2022-06-27 22:37:10 +0700

    add ui card

--- src/pages/allTask/index.js
+++ src/pages/allTask/index.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import Card from '../../components/card'
+import './style.scss'
 
 export default function AllTask() {
   return (
-    <div>AllTask</div>
+    <div className='all-task'>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
   )
 }
