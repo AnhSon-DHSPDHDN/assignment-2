bcb2d59f6c95061031dcb1ba89e9488f95780f34
sontva-2630 <tran.van.anh.son@sun-asterisk.com>
2022-06-28 10:43:04 +0700

    ref create full UI

--- src/pages/createTask/index.js
+++ src/pages/createTask/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import FormTask from '../../form'
 
 export default function CreateTask() {
   return (
-    <div>CreateTask</div>
+    <div style={{
+      padding: '20px',
+      height: 'max-content',
+      width: '100%',
+      display: 'flex',
+      flexWrap: 'wrap',
+      gap: '20px'
+    }}>
+      <FormTask />
+    </div >
   )
 }
