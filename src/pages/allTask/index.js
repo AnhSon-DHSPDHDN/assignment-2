3083617f04d738ef5ad08b88b134cc6ec70eb671
sontva-2630 <tran.van.anh.son@sun-asterisk.com>
2022-06-28 14:06:20 +0700

    config redux

--- src/pages/allTask/index.js
+++ src/pages/allTask/index.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import Card from '../../components/card'
+import { actGetAllTasks } from '../../redux/actions/tasksListAct'
 import './style.scss'
 
 export default function AllTask() {
+  const tasksList = useSelector(state => state?.tasks?.tasksList)
+  const dispatch = useDispatch()
+  console.log(tasksList);
+
+  useEffect(() => {
+    dispatch(actGetAllTasks())
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <div className='all-task'>
       <Card />
---
+++ src/redux/reducers/taskReducer.js
@@ -0,0 +1,16 @@
+import { TASKS_TYPE } from "../../constants/tasksListType"
+
+const initialState = {
+  tasksList: [],
+  taskDetail: {},
+}
+
+export const tasksListReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case TASKS_TYPE.GET_ALL_TASKS:
+      console.log('hiha');
+      return { ...state }
+    default:
+      return state
+  }
+}
